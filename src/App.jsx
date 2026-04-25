a55be2f235089beb96e93fe1626280ce3fe465f0
breoner <doobikbogdan@gmail.com>
2026-04-25 09:54:27 +0300

    fix

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Section from './components/Section/Section'
+import Title from './components/Title/Title'
+
+function App() {
+  return (
+    <>
+    <Section>
+      <Title/>
+    </Section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,9 @@
+import style from "./Title.module.css"
+
+function Title() {
+    return (
+        <h1 className={style.title}>Hello</h1>
+    )
+}
+
+export default Title
